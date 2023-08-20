cac9ed4eaf86bf589a068f89f6cd41aa344e6177
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-08-20 07:47:40 -0700

    task done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print from 0 to 9
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char bo = bo + 0;
+
+	while (bo < 10)
+	{
+		putchar(bo + '0');
+			bo++;
+	}
+	return (0);
+}
