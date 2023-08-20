32463b42bdb3a6920065759567705228bc5a4716
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-08-20 07:18:07 -0700

    task done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print from 0 to 9
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int bo = 0;
+
+	while (bo < 10)
+	{
+		putchar(bo);
+		bo++;
+	}
+	return (0);
+
+}
