26497551b1cd595daffcca883198a0afe10f741c
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-09-07 14:23:47 -0700

    Task done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphapet
+ * Return: (0) sccess
+*/
+
+void print_alphabet(void)
+{
+	for (int j = 'n'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	return (0);
+}
