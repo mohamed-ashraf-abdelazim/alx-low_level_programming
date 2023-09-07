13f58e7beaf1a52194948d87cf6a6adeffef8961
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-09-07 14:59:53 -0700

    Task done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks if the letter is lowercase alphapet
  * @c: parameter
- * Return: 1 is lowercase 0 is not
+ * Return: (1) is lowercase (0) is not
 */
 
 int _islower(int c)
