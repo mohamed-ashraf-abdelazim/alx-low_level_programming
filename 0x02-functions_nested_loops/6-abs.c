75b13e791720bca7b68257825d7075950c692ec6
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-09-07 16:29:49 -0700

    Task done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of number
+ * @n: parameter
+ * Return: 0 sucess
+*/
+
+int _abs(int n)
+{
+	if (n >= '0')
+		_putchar(n);
+	else
+	{
+		z = n * (-1);
+		_putchar(z);
+	}
+	return (0);
+}
