89f3914f3b961b3ac52adc8670660c00881f61b5
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-09-07 15:21:10 -0700

    Task done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if c alphapet lower or highe case
+ * @c: parameter
+ * Return: (1) is alphapet (0) is not
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
