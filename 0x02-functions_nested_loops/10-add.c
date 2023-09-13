ee532fcca23ac4ec4af8c401d55e5b5df53045bc
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-09-12 21:42:06 -0700

    Task done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+ * add - adds two numbers and returns its value
+ * @a: number
+ * @b: number
+ * Return: z
+ */
+
+int add(int a, int b)
+{
+	int z = a + b;
+
+	return (z);
+}
