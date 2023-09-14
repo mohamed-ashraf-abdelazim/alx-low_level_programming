dfebfe68120524873418bc854785c0a75e863db5
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-09-13 22:56:03 -0700

    Task compelete

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,10 +9,12 @@ int main(void)
 {
 	int i;
 
-	/*
-	 * printf("Infinite loop incoming :(\n");
-	 * i = 0;
-	 * while (i < 10)
+	printf("Infinite loop incoming :(\n");
+	 
+	 i = 0;
+	 
+	 /*
+	  * while (i < 10)
 	{
 		putchar(i);
 		}
