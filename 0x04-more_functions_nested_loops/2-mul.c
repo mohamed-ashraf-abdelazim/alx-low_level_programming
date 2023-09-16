3d3353340de6564117c58957fbb4d65010a69363
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-09-16 00:46:08 -0700

    Task done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - prints the multiplication of two numbers
+ * @a: parameter
+ * @b: parameter
+ * Return: 0 success
+*/
+
+int mul(int a, int b)
+{
+	int z;
+
+	z = a * b;
+	_putchar(z);
+	return (0);
+}
