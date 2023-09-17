68f3a8950cf9662a36c37eef9f3fb5b6f88ef22a
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-09-16 23:05:34 -0700

    Task done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints from 0 to 14 ten times
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j / 10);
+			_putchar(i % 10);
+			if ((i % 10) == 0)
+				continue;
+		}
+		_putchar('\n');
+	}
+}
