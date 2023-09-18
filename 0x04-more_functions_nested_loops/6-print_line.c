098a378f74c6e9231883df114c9831e10aee7599
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-09-17 20:46:10 -0700

    Task done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints line with long n
+ * @n: length of the line
+*/
+
+void print_line(int n)
+{
+	int num;
+
+	for (num = 0; num <= n; num++)
+	{
+		_putchar('_');
+		if (n <= 0)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
