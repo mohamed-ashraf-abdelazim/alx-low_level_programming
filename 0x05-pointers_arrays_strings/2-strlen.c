15fe29ace675ca38ae66810c3dc21426bc27ccbd
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-09-27 09:26:51 -0700

    Task done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - count the numbers of string character
+ * @s: pointer
+ * Return: 0 is success
+*/
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+		count++;
+
+	return (counter);
+}
