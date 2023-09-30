14d5361c7108b7daba7afc2b40abc0ba4d6cc3de
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-09-29 18:11:15 -0700

    Task Done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,8 +8,12 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		_putchar('\n');
-	_putchar(*s);
-	s++;
-	_puts_recursion(char *s);
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
