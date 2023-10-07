8e403b62b5bd2089b74b7d5126124c848fc81429
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-10-06 21:57:19 -0700

    Task done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ * argc: num of arguments
+ * argv: string of arguments
+ * Return: 0 success
+*/
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
