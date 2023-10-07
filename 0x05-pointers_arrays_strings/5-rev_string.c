9af86288485831f3169b98cf7bf42c22bb4211ab
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-10-06 18:14:43 -0700

    Task done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,19 +7,17 @@
 
 void rev_string(char *s)
 {
-	int length, i, middle;
+	int l, i;
 	char temp;
 
-	middle = length / 2;
-
 	/* calculates the length of string */
-	for (length = 0; *s != '\0'; s++)
-		 length++;
+	for (l = 0; s[l] != '\0'; l++)
+		;
 	/* reverse the string by swapping it*/
-	for (i = 0; i < middle; i++)
+	for (i = 0; i < l / 2; i++)
 	{
-		s[i] = temp;
-		s[i] = s[length - i - 1];
-		s[length - i - 1] = temp;
+		temp = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = temp;
 	}
 }
