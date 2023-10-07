f88336623db3fe9679a969ccd35b4f5dfce1b678
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-10-06 23:17:05 -0700

    Task done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 
 /**
- * */
+ * main - sum of args
+ * @argc: num of arg
+ * @argv: str of arg
+ * Return: 0 success
+*/
 
-int main(int argc, char)
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc == 1)
+		printf("0\n");
+	if (argv[i] >= 64)
+		printf("Error\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+			printf("%d\n", sum);
+		}
+	}
+	return (0);
+}
