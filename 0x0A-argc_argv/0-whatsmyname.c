2d5ea7ff1c5dc279af2cd2758407479301994c90
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-10-06 22:02:20 -0700

    Task done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,14 @@
 
 /**
  * main - prints the program name
- * argc: num of arguments
- * argv: string of arguments
+ * @argc: num of arguments
+ * @argv: string of arguments
  * Return: 0 success
 */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
