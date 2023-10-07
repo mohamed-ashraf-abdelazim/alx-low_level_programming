77329a11261803bcb4c96f8be7ff9c1184c13f96
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-10-06 17:07:21 -0700

    Task done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two numbers
+ * @a: parameter
+ * @b: parameter
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	*a = temp;
+	*a = *b;
+	*b = temp;
+}
