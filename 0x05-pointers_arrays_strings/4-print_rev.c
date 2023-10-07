68e5a2384bd56d932154c2736698eef304626437
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-10-06 17:23:19 -0700

    Task done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: parameter
+*/
+
+void print_rev(char *s)
+{
+	for (; *s != '\0'; s++)
+	;
+	for (; *s != '\0'; s--)
+		_putchar(*s);
+}
