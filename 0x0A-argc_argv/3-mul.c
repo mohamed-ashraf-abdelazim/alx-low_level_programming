d93298b65bc580d6726aabc0b93c9846df2edfe2
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-10-06 22:51:46 -0700

    Task done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplay two arguments
+ * @argc: num of arg
+ * @argv: str of srg
+ * Return: 0 success 1 failure
+*/
+
+int main(int argc, int *argv[])
+{
+	int mult;
+
+	if (argc == 3)
+	{
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mult);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
