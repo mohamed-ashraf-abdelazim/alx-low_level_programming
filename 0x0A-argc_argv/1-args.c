f5078c2033b6cd64a964dab070efe49d2518386c
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-10-06 22:09:53 -0700

    Task done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints num of arguments
+ * @argc: num of arguments
+ * @argv: string of the arguments
+ * Return: 0 success
+*/
+
+int main(int argc, char *argv)
+{
+	printf("%i\n", argc);
+	return (0);
+}
