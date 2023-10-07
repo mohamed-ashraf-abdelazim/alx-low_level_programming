bd2df2af9ab6a5f82718b266ed6834af4489b94c
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-10-06 19:56:24 -0700

    Task done

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,25 +7,12 @@
 
 void puts_half(char *str)
 {
-	int i, len, middle, n;
+	int count, i, n;
 
-	for (len = 0; *str != '\0'; str++)
-		len++;
-	n = (len - 1) / 2;
-	middle = len / 2;
-	if (len % 2 == 0)
-	{
-		for (i = 0 ; i > middle; i++)
-			_putchar(str[i]);
-		_putchar('\n');
-	}
-	else
-	{
-		len = len - 1;
-		for (i = 0 ; i > middle; i++)
-			_putchar(str[i]);
-		_putchar(n);
-		_putchar('\n');
-
-	}
+	for (; str[i] != '\0'; i++)
+		count++;
+	n = (count - 1) / 2;
+	for (i = n + 1; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
