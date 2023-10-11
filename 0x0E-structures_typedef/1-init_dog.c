cf44cdcaf31e726a82106bc5bd5793233a81ba8a
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-10-11 15:48:36 -0700

    Task done

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - changes the parameters
+ * @d: pointer to struct
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
