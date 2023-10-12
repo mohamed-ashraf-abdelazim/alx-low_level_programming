635ff1e1e56acfa5c07a444b4e4e7e804282dac7
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-10-12 08:18:24 -0700

    Task done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	int *end = array + size - 1;
 
 	if (array && size && action)
-		while (size <= end)
+		while (array <= end)
 			action(*array++);
 
 }
