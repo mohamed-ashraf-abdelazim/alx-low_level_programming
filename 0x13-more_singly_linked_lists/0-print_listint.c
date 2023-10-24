dfae3b791f417ed95b232f1c740c255304c181bd
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-10-24 13:51:06 -0700

    Task Done

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: points at linked list listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nod = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		nod++;
+		h = h->next;
+	}
+
+	return (nod);
+}
