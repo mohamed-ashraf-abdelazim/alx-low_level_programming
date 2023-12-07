01f8439d34b335e95c0dbd2e5300e89f327e8d7c
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-12-07 08:08:03 -0800

    Task done

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_grid - frees memory in prevoius function
+ * @grid: parameter
+ * @height: heigt
+ * Return: nothing
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		while (i--)
+			free(grid[i]);
+		free(grid);
+	}
+}
