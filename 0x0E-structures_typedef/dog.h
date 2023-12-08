572c0930a827a4e4a259768347cecd77112609c4
mohamed-ashraf-abdelazim <mohamed.ashraf.abdelazim.mohamed@gmail.com>
2023-12-08 13:06:50 -0800

    Task done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - record for dog
+ * @name: name
+ * @age: dog age
+ * @owner: owner name
+ * Description: dog profile
+*/
+
+typedef struct
+{
+	char *name;
+	float age;
+	char *owner;
+
+} dog;
+
+
+#endif
